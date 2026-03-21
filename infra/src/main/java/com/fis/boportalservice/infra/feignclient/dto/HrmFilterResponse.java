83148a54017c81151e432d704f63ad0db0e042a9
vominhdat68 <datminhvo0901@gmail.com>
2026-03-21 19:20:19 +0700

    fix: support HRM filter response deserialization

--- infra/src/main/java/com/fis/boportalservice/infra/feignclient/dto/HrmFilterResponse.java
+++ infra/src/main/java/com/fis/boportalservice/infra/feignclient/dto/HrmFilterResponse.java
@@ -1,10 +1,14 @@
 package com.fis.boportalservice.infra.feignclient.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class HrmFilterResponse {
   Integer no;
   Long userId;
